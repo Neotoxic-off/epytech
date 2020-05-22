c088d0d8449315028a83743b8158a366dc98066e
Neo <paul.gardien@epitech.eu>
2020-05-22 23:11:25 +0200

    Update 0.2: change the class

--- epitech.py/attempt.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def do(session):
-    result = session.get("https://intra.epitech.eu/login?format=json")
-    data = result.json()
-    return(data)
--- epitech.py/session.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def create(link):
-    session = requests.Session()
-    session.get(link)
-    return (session)
